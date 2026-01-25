bc1018541aa02ac02f103d494a211c21444450a4
jessielw <jessielw4049@gmail.com>
2026-01-25 14:01:33 -0500

    feat: add util

---
+++ frontend/src/lib/utils/date.ts
@@ -0,0 +1,9 @@
+function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+export { formatDate };
