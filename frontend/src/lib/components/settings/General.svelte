ab14808c4e7a3cdb507b18625b0476b0e186a589
jessielw <jessielw4049@gmail.com>
2026-02-15 23:42:41 -0500

    feat: place holder for general

---
+++ frontend/src/lib/components/settings/General.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Component } from "svelte";
+
+  interface Props {
+    svgIcon: Component | null;
+  }
+  let { svgIcon }: Props = $props();
+</script>
+
+<div>
+  <div>
+    <h2 class="flex items-center gap-3 text-xl font-semibold text-foreground">
+      {console.log(svgIcon)}
+      {#if svgIcon}
+        {@const Icon = svgIcon}
+        <Icon class="size-5" aria-hidden="true" />
+      {/if}
+      <span class="align-middle">General</span>
+    </h2>
+    <!-- <p class="text-sm text-muted-foreground mt-1">
+      About Reclaimerr, links to docs, support, etc.
+    </p> -->
+  </div>
+  Placeholder for general page.
+</div>
