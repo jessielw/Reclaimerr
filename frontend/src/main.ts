e7a21b15b0002318a746c7210b41f71058f551ab
jessielw <jessielw4049@gmail.com>
2026-03-08 18:50:49 -0400

    refactor: kebab

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import "./app.css";
 import { mount } from "svelte";
-import App from "./App.svelte";
+import App from "./app.svelte";
 
 const app = mount(App, {
   target: document.getElementById("app")!,
