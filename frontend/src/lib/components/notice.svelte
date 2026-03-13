aa51b2f222eec60fa74f8d008b64bc457ac43a6d
jessielw <jessielw4049@gmail.com>
2026-03-13 11:31:45 -0400

    refactor: remove un-needed code feat: accept class feat: DRY

--- frontend/src/lib/components/notice.svelte
+++ frontend/src/lib/components/notice.svelte
@@ -12,55 +12,46 @@
         icon: typeof InfoIcon | typeof TriangleAlert | typeof Bug;
         class: string;
       };
-      bg: string;
-      border: string;
-      text: string;
     }
   > = {
     info: {
-      main: "bg-primary/10 border border-primary/70 text-foreground/75",
+      main: "bg-primary/10 border-primary/70",
       header: "bg-primary/12",
       icon: {
         icon: InfoIcon,
         class: "text-foreground/75 size-4",
       },
-
-      bg: "bg-primary/10",
-      border: "border-primary/70",
-      text: "text-foreground",
     },
     warning: {
-      main: "bg-call-to-action/10 border border-call-to-action/70 text-foreground/75",
+      main: "bg-call-to-action/10 border-call-to-action/70",
       header: "bg-call-to-action/12",
       icon: {
         icon: TriangleAlert,
         class: "text-foreground/75 size-4",
       },
-
-      bg: "bg-call-to-action/10",
-      border: "border-call-to-action/70",
-      text: "text-foreground",
     },
     error: {
-      main: "bg-warning/10 border border-warning/70 text-foreground/75",
+      main: "bg-warning/10 border-warning/70",
       header: "bg-warning/12",
       icon: {
         icon: Bug,
         class: "text-foreground/75 size-4",
       },
-
-      bg: "bg-warning/10",
-      border: "border-warning/70",
-      text: "text-foreground",
     },
   };
 
   interface Props {
     title?: string;
     type?: "info" | "warning" | "error";
+    class?: string;
     children?: any;
   }
-  let { title = "Info", type = "info", children = undefined }: Props = $props();
+  let {
+    title = "Info",
+    type = "info",
+    class: className = "",
+    children = undefined,
+  }: Props = $props();
 
   const IconComponent = $derived.by(() => {
     return types[type].icon.icon;
@@ -68,8 +59,8 @@
 </script>
 
 <div
-  class={`flex flex-col border ${types[type].main} overflow-hidden
-  rounded-sm shadow`}
+  class={`flex flex-col border ${types[type].main} text-foreground/75 overflow-hidden
+    rounded-sm shadow ${className}`}
 >
   <span class={`flex items-center gap-2 ${types[type].header} p-1 pl-2.5`}>
     <IconComponent class={types[type].icon.class} />
