f27a100a8b9706581054ee3e62ac31c810e665a9
jessielw <jessielw4049@gmail.com>
2026-03-15 17:15:55 -0400

    refactor: allow splitChar arg

--- frontend/src/lib/utils/strings.ts
+++ frontend/src/lib/utils/strings.ts
@@ -3,10 +3,10 @@
  * @param str
  * @returns
  */
-const toTitleCase = (str: string): string => {
+const toTitleCase = (str: string, splitChar: string = " "): string => {
   return str
     .toLowerCase()
-    .split(" ")
+    .split(splitChar)
     .map((word: string) => {
       if (word.length === 0) return "";
       return word.charAt(0).toUpperCase() + word.slice(1);
