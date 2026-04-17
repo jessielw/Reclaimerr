dc5be07025d584086e6a0f06ad194b0fd4055107
jessielw <jessielw4049@gmail.com>
2026-04-17 18:06:31 -0400

    feat: v0.1.0-beta8

--- backend/core/__version__.py
+++ backend/core/__version__.py
@@ -3,4 +3,4 @@ from semver import VersionInfo
 program_name = "Reclaimerr"
 program_url = "https://github.com/jessielw/reclaimerr"
 
-__version__ = VersionInfo.parse("0.1.0-beta7")
+__version__ = VersionInfo.parse("0.1.0-beta8")
--- frontend/src/lib/version.ts
+++ frontend/src/lib/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = "0.1.0-beta7";
+export const VERSION = "0.1.0-beta8";
