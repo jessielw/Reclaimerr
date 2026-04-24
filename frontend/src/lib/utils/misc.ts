efd74f6076b9e6d29412745c2eeac79ee3b7f38a
jessielw <jessielw4049@gmail.com>
2026-04-24 12:52:17 -0400

    feat: upgrade to support elements

--- frontend/src/lib/utils/misc.ts
+++ frontend/src/lib/utils/misc.ts
@@ -1,17 +1,22 @@
+type ScrollTarget = MouseEvent | Element;
+
 /**
  * Utility function to smoothly scroll to an element when a link is clicked
  * @param e The mouse event triggered by clicking the link
  * @returns void
  */
-const scrollIntoView = (e: MouseEvent, highlight: boolean = false) => {
-  e.preventDefault(); // prevent default anchor behavior
-  const el = document.querySelector(
-    (e.currentTarget as HTMLAnchorElement).getAttribute("href") || "",
-  );
+const scrollIntoView = (target: ScrollTarget, highlight: boolean = false) => {
+  let el: Element | null = null;
+  if (target instanceof Element) {
+    el = target;
+  } else if (target instanceof MouseEvent) {
+    target.preventDefault();
+    el = document.querySelector(
+      (target.currentTarget as HTMLAnchorElement).getAttribute("href") || "",
+    );
+  }
   if (!el) return;
-  el.scrollIntoView({
-    behavior: "smooth",
-  });
+  el.scrollIntoView({ behavior: "smooth" });
   if (highlight) {
     el.classList.add("border-3", "border-call-to-action");
     setTimeout(() => {
